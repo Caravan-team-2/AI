d3422e504da3819518d0bd99d3363f3f17221f8d
moundher122 <m.bouroumana@esi-sba.dz>
2025-09-20 11:27:53 +0100

    finish

--- fraud/handler/license_id.py
+++ fraud/handler/license_id.py
@@ -1,8 +1,11 @@
 from fraud.kafka.producer import publish
+import numpy as np
+from fraud.models.detect_driver_license import detect_driver_license
 class LicenseIdHandler:
     def __init__(self):
         pass
     @staticmethod
-    def handle(license_id: str) -> bool:
-        publish({"license_id": license_id}, topic="license_id")
-        return True
+    def handle(image: np.array) -> bool:
+        LicenseId=detect_driver_license(license_id)
+        publish({"license_id": LicenseId}, topic="license_id")
+        return LicenseId
--- fraud/handler/plate_detcet.py
+++ fraud/handler/plate_detcet.py
@@ -1,5 +1,8 @@
+from fraud.models.plate_detect import LicensePlateDetector
+import numpy as np
 class PlateDetectHandler:
     def __init__(self):
         pass
-    def handle(self, plate_detection: str) -> None:
+    @staticmethod
+    def handle(plate_detection:np.array) -> None:
      print(f"Handling plate detection: {plate_detection}")
